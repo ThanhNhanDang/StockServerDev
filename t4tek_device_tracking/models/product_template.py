0fb3abb5e4e19e417c2ac3a43942c6e8fc2f113f
giangt4tek <GiangNT@int.t4tek.co>
2025-08-27 16:37:39 +0700

    tracking forcus only serial in product

--- t4tek_device_tracking/models/product_template.py
+++ t4tek_device_tracking/models/product_template.py
@@ -7,3 +7,35 @@ _logger = logging.getLogger(__name__)
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     product_type_id = fields.Many2one('product.type', string='Loại sản phẩm')
+
+    # tracking = fields.Selection(
+    #     selection=[('serial', 'By Unique Serial Number')],
+    #     string="Tracking",
+    #     default='serial',
+    #     required=True
+    # )
+
+    
+    @api.onchange('tracking')
+    def _onchange_tracking(self):
+        if self.tracking != 'serial':
+            self.tracking = 'serial'
+        else:
+            raise UserError(_("Chỉ được phép chọn 'serial' cho sản phẩm này."))
+
+
+        
+    @api.model
+    def write(self, vals):
+        # Nếu có logic với is_storable thì thêm ở đây
+        if "is_storable" in vals and not vals["is_storable"]:
+            # Nếu không cho lưu kho thì buộc tracking về none
+            vals["is_storable"] = True
+        # Nếu có truyền field tracking
+        if "tracking" in vals:
+            # Chỉ cho phép các giá trị hợp lệ
+            valid_values = ["none", "lot", "serial"]
+            if vals["tracking"] != "serial":
+                # Nếu sai thì đưa về default (none)
+                vals["tracking"] = "serial"
+        return super(ProductTemplate, self).write(vals)
